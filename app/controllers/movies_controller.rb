94e7a4c3a0795369eb5896936c3064b2fd3aaae1
EC2 Default User <ec2-user@ip-172-31-38-104.us-east-2.compute.internal>
2019-09-28 20:50:46 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,45 +11,11 @@ class MoviesController < ApplicationController
   end
 
   def index
-    
-    redirect = false
     if params[:sort]
       @sorting = params[:sort]
-    elsif session[:sort]
-      @sorting = session[:sort]
-      redirect = true 
+      # sort using model method
     end
-    
-    # access ratings from model Movie
-    @all_ratings = Movie.all_ratings
-    if params[:ratings]
-      @ratings = params[:ratings]
-    elsif session[:ratings]
-      @ratings = session[:ratings]
-      redirect = true
-    else
-      @all_ratings.each do |rat|
-        (@ratings ||= { })[rat] = 1
-      end
-      redirect = true
-    end
-    
-    # redirect to /index
-    if redirect
-      redirect_to movies_path(:sort => @sorting, :ratings => @ratings)
-    end
-    
-    #  access checked item
-    if session[:ratings]
-      @checked_ratings = session[:ratings].keys
-    elsif
-      @checked_ratings = @all_ratings
-    end
-    
-    # Sort By title or release date.
-    @movies = Movie.order(@sorting).where(rating: @checked_ratings)
-  	session[:sort] = @sorting
-  	session[:ratings] = @ratings
+    @movies = Movie.order(@sorting)
   end
 
   def new
